2ad58baf57b1eaa20df01f533925c9d5b57e61ad
Bruce Baron <baron.br@gmail.com>
2019-09-26 18:48:07 -0400

    cp chapter12.02-calendar to /boot

--- boot/src/main/resources/database/h2/security-metadata-data.sql
+++ boot/src/main/resources/database/h2/security-metadata-data.sql
@@ -3,5 +3,5 @@ insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('
 insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('/signup/*','permitAll',30);
 insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('/logout','permitAll',40);
 insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('/errors/**','permitAll',50);
-insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('/admin/**','hasRole("ROLE_ADMIN")',60);
+insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('/admin/**','local and hasRole("ROLE_ADMIN")',60);
 insert into security_filtermetadata(ant_pattern,expression,sort_order) values ('/**','hasRole("ROLE_USER")',70);
